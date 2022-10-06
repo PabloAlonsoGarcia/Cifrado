b3f2db5634d63de7c67ba64baf4961b8580f75ec
PabloAlonsoGarcia <pabalo172@alu.centroafuera.es>
2022-10-06 14:20:25 +0200

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,35 @@
+fun main() {
+    var cadena: String=""
+
+    print("Introduce la cadena a ")
+    cadena=readln().toString()
+
+    print
+
+
+    //cifrarCadenaCesar(cadena)
+
+}
+
+/*fun cifrarCadenaCesar(cadena: String){
+
+    val arrayC = ArrayList<Char>()
+    var cont: Int=0
+
+
+    cadena.forEach {
+        if(!it.isWhitespace()){
+            arrayC[cont]=it
+            cont++
+        }
+
+    }
+    cont=0
+    arrayC.forEach(){
+        arrayC[cont]
+
+    }
+
+*/
+
+}
